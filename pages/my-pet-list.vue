797178bb4bc942f4241df5c8e45a4de0a478a168
Aljohn19977 <aljohnmirandilla@gmail.com>
2020-03-23 21:05:32 +0800

    Mar 23

---
+++ pages/my-pet-list.vue
@@ -0,0 +1,70 @@
+<template>
+    <div>
+        <!-- navbar -->
+        <div class="navbar navbar-pages">
+            <div class="container">
+                <div class="content">
+                    <h4>
+                        <router-link to="/" class="link-back">
+                            <i class="fa fa-arrow-left waves-effect waves-light-grey"></i>
+                        </router-link>
+                        My Pet</h4>
+                </div>
+            </div>
+        </div>
+        <!-- end navbar -->
+        <!-- list -->
+        <div class="list segments-page">
+            <div class="container">
+                <div class="content waves-effect waves-light-grey" style="width:100%">
+                    <div class="list-img">
+                        <img src="https://www.petmd.com/sites/default/files/Acute-Dog-Diarrhea-47066074.jpg" alt="">
+                    </div>
+                    <div class="list-text">
+                        <h6>Franky</h6>
+                        <p>My happy dog</p>
+                    </div>
+                </div>
+                <div class="content waves-effect waves-light-grey" style="width:100%">
+                    <div class="list-img">
+                        <img src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/smartest-dog-breeds-1553287693.jpg?crop=0.671xw:1.00xh;0.167xw,0&resize=640:*" alt="">
+                    </div>
+                    <div class="list-text">
+                        <h6>Nikki</h6>
+                        <p>My bad dog</p>
+                    </div>
+                </div>
+                <div class="content waves-effect waves-light-grey" style="width:100%">
+                    <div class="list-img">
+                        <img src="https://www.washingtonpost.com/resizer/uwlkeOwC_3JqSUXeH8ZP81cHx3I=/arc-anglerfish-washpost-prod-washpost/public/HB4AT3D3IMI6TMPTWIZ74WAR54.jpg" alt="">
+                    </div>
+                    <div class="list-text">
+                        <h6>Sky</h6>
+                        <p>The Destroyer</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- end list -->
+
+
+    </div>
+    <!-- end home login -->
+</template>
+
+<script>
+export default {
+  middleware: ["auth"],
+  data() {
+
+  },
+  async asyncData({ $axios }) {
+
+  },
+  created() {
+
+  },
+  methods: {
+  }
+};
+</script>
--- pages/pet.vue
+++ pages/pet.vue
@@ -5,9 +5,9 @@
             <div class="container">
                 <div class="content">
                     <h4>
-                        <a href="" class="link-back">
+                        <router-link to="/" class="link-back">
                             <i class="fa fa-arrow-left waves-effect waves-light-grey"></i>
-                        </a>
+                        </router-link>
                         Add Pet</h4>
                 </div>
             </div>
@@ -20,17 +20,18 @@
 				<div class="col s12">
 					<div class="wrap-content plus-pb b-shadow">
 						<div class="wrap-title">
-							<h5>Pet Photo</h5>
+							<h5>Pet Profile Picture</h5>
 						</div>
 						<div class="content">
-							<img src="images/profile.png" alt="">
+							<img src="images/profile.png" v-if="form.image == null" alt="">
+                            <img :src="form.image" v-else alt="">
 							<div class="button-upload">
-								<form>
-									<input id="button-file" type="file">
+			
+									<input id="button-file" type="file" accept="image/*" ref="fileInput" @change="onFilePicked">
 									<label for="button-file">
 										<span class="b-shadow">Upload</span>
 									</label>
-								</form>
+						
 							</div>
 						</div>
 					</div>
@@ -43,20 +44,26 @@
         <div class="reservation segments-page" style="padding-top:0px">
             <div class="container">
                 <div class="content-form b-shadow">
-                    <form>
+                  	<form @submit.prevent="submit">
                         <div class="input-field">
                             <input v-model="form.name" type="text" id="name">
                             <label for="name">Name</label>
                         </div>
-                        <div class="input-field">
-                            <date-picker type="date" v-model="form.birthdate" placeholder="Date of Birth" id="birthdate" style="min-width:100%;"></date-picker>
-                        </div>
                         <div>
                             <label class="typo__label">Breed</label>
-                            <multiselect @select="toggleSelected"  v-model="value" deselect-label="Can't remove this value" track-by="name" label="name" placeholder="Select one" :options="options" :searchable="true" :allow-empty="false">
+                            <multiselect @select="toggleSelected"  v-model="value" deselect-label="" track-by="name" label="name" placeholder="Select one" :options="options" :searchable="true" :allow-empty="false">
                                 <template slot="singleLabel" slot-scope="{ option }">{{ option.name }}</template>
                               </multiselect>
                         </div>
+                        <div style="padding-top:10px">
+                            <label class="typo__label">Gender</label>
+                            <multiselect @select="toggleSelected"  v-model="genderValue" deselect-label="" track-by="name" label="name" placeholder="Select one" :options="gender" :allow-empty="false">
+                                <template slot="singleLabel" slot-scope="{ option }">{{ option.name }}</template>
+                              </multiselect>
+                        </div>
+                        <div class="input-field">
+                            <date-picker type="date" v-model="form.birthdate" placeholder="Date of Birth" id="birthdate" style="min-width:100%;"></date-picker>
+                        </div>                                    
                         <div class="input-field">
                             <p style="padding-top:10px">Playful</p>
                             <vue-slider v-model="range" id="range"/>
@@ -77,7 +84,7 @@
                             <textarea v-model="form.description" id="message" class="materialize-textarea" cols="30" rows="30"></textarea>
                             <label for="message">Description</label>
                         </div>
-                        <button class="button waves-effect waves-light">Submit</button>
+                        <button class="button waves-effect waves-light" type="submit">Submit</button>
                     </form>
                 </div>
             </div>
@@ -89,39 +96,73 @@
 </template>
 
 <script>
-    export default {
-        middleware: ["auth"],
-        data() {
-            return {
-                form: {
-                    birthdate: "",
-                    name:"",
-                    description:"",
-                    breed:"",
-                    user_id: this.$store.getters.user.id
-                },
-                breeds: [],
-                test: [],
-                options: [],
-                value: null,
-                range: 0
-            };
+export default {
+  middleware: ["auth"],
+  data() {
+    return {
+      form: {
+        birthdate: "",
+        image: null,
+        name: "",
+        gender: "",
+        description: "",
+        breed: "",
+        user_id: this.$store.getters.user.id
+      },
+      breeds: [],
+      test: [],
+      gender: [
+        {
+          name: "Male",
+          id: "male"
         },
-        async asyncData({$axios}) {
-            let data = await $axios.$get("https://api.thedogapi.com/v1/breeds");
-            return {breeds: data};
-        },
-        created() {
-            this.breeds.forEach((item) => {
-                this.options.push({name:item.name,id:item.id});
-            });
-        },
-        methods: {
-            toggleSelected(value, id) {
-                this.form.breed = value.name
-                }
-
+        {
+          name: "Female",
+          id: "female"
         }
+      ],
+      options: [],
+      value: null,
+      genderValue: null,
+      range: 0
     };
+  },
+  async asyncData({ $axios }) {
+    let data = await $axios.$get("https://api.thedogapi.com/v1/breeds");
+    return { breeds: data };
+  },
+  created() {
+    this.breeds.forEach(item => {
+      this.options.push({ name: item.name, id: item.id });
+    });
+  },
+  methods: {
+    async submit() {
+      await this.$axios
+        .$post("http://pawbookserverapi.test/api/addpet", this.form)
+        .then(function(response) {
+          console.log(response);
+        })
+        .catch(function(error) {
+          console.log(error);
+        });
+    },
+    toggleSelected(value, id) {
+      this.form.breed = value.name;
+    },
+    onFilePicked(event) {
+      const files = event.target.files;
+      let filename = files[0].name;
+      if (filename.lastIndexOf(".") <= 0) {
+        return alert("Please add a valid file! ");
+      }
+      const fileReader = new FileReader();
+      fileReader.addEventListener("load", () => {
+        this.form.image = fileReader.result;
+      });
+      fileReader.readAsDataURL(files[0]);
+    }
+  }
+};
 </script>
 <style src="vue-multiselect/dist/vue-multiselect.min.css"></style>
